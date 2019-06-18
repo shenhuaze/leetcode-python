4a5a2d8dec052b28b142ed276ffb0fb30b67513e
shenhuaze <shenhuaze@pku.edu.cn>
2019-06-18 22:04:00 +0800

    update code

---
+++ code/string_to_integer.py
@@ -0,0 +1,31 @@
+
+def my_atoi(str):
+    if str is None or len(str.strip()) == 0:
+        return 0
+    sign = 1
+    base = 0
+    i = 0
+    n = len(str)
+    while i < n and str[i] == ' ':
+        i += 1
+    if i < n and (str[i] == '-' or str[i] == '+'):
+        if str[i] == '-':
+            sign = -1
+        i += 1
+    max_int = 2**31 - 1
+    min_int = -2**31
+    while i < n and '0' <= str[i] <= '9':
+        if base > max_int // 10 or (base == max_int // 10 and ord(str[i]) - ord('0') > 7):
+            if sign == 1:
+                return max_int
+            else:
+                return min_int
+        base = base * 10 + ord(str[i]) - ord('0')
+        i += 1
+    return sign * base
+
+
+if __name__ == "__main__":
+    str_ = "  -43"
+    print(my_atoi(str_))
+
